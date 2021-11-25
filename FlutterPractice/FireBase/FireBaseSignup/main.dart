b122e38d5898492370e3ce70cceac73231126bf6
Saon00 <57843701+Saon00@users.noreply.github.com>
2021-11-25 09:26:59 +0600

    Add files via upload

---
+++ FlutterPractice/FireBase/FireBaseSignup/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutterpractice/signup_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Firebase'),
+        ),
+        body: SignUpPage(),
+      ),
+    );
+  }
+}
